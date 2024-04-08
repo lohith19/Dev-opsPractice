414a99671293f7b2c7b64f9045df2ef2c6464793
Lohith Sai Velagapudi <lohithsai.velagapudi@dotdashmdp.com>
2024-04-08 09:54:04 +0530

    error correction

--- Instancecreation.sh
+++ Instancecreation.sh
@@ -13,7 +13,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-IPADDRESS=aws ec2 run-instances --image-id $AMI  --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
+IPADDRESS= $(aws ec2 run-instances --image-id $AMI  --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 echo " $i: $IPADDRESS"
 
 #route53 records
